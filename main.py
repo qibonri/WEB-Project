daa1c26f69735f5cc2d7eb20430a4defcd1d0e29
qibonri <75024325+qibonri@users.noreply.github.com>
2021-05-03 19:48:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def main():
             abort(404)
         return redirect('/')
 
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=5000, host='0.0.0.0')
 
 
 if __name__ == '__main__':
